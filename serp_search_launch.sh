bff1f072bd3de1c4fb743a45b1e54e8f2791649a
kacey-yang <kacey_yang@ymail.com>
2026-03-21 23:03:58 +0000

    Update SERP API key in serp_search_launch.sh

--- serp_search_launch.sh
+++ serp_search_launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 search_url=https://serpapi.com/search
-serp_api_key="252eaa31091c32b34d36184f2a1c17a9647bce6cfebdab837c13137dad17f1bd" # put your serp api key here (https://serpapi.com/)
+serp_api_key="f82ece71053979e5234ca5f1ecc48bd31c55b2e28c2eae26d0b3439322b8580b" # put your serp api key here (https://serpapi.com/)
 
 python search_r1/search/serp_search_server.py --search_url $search_url --topk 3 --serp_api_key $serp_api_key
