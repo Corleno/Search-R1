43f15c19e00690f0b1788be25869b6cb58a7724d
kacey-yang <kacey_yang@ymail.com>
2026-03-21 23:48:07 +0000

    Update serp_search_launch.sh to use environment variable for SERP API key

--- serp_search_launch.sh
+++ serp_search_launch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+# Usage: export SERPAPI_API_KEY='your_key'   then:  bash serp_search_launch.sh
 
 search_url=https://serpapi.com/search
-serp_api_key="f82ece71053979e5234ca5f1ecc48bd31c55b2e28c2eae26d0b3439322b8580b" # put your serp api key here (https://serpapi.com/)
+serp_api_key="${SERPAPI_API_KEY:?Please export SERPAPI_API_KEY (https://serpapi.com/)}"
 
 python search_r1/search/serp_search_server.py --search_url $search_url --topk 3 --serp_api_key $serp_api_key
